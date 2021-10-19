3903ae849675bf724dc5f122b233c8d4e4454433
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-10-19 17:57:35 +0200

    finished Afgb2

--- pr1_blatt1/Umwandlung.java
+++ pr1_blatt1/Umwandlung.java
@@ -2,11 +2,22 @@ package pr1_blatt1;
 
 public class Umwandlung {
   public static void main(String[] args) {
-    // System.out.println(Integer.toHexString(80));
-    //radix 10 is for decimal number, for hexa use radix 16
-    int radix = 10;
-    int value_int = 80;
-    char value_char = Character.forDigit(value_int , radix);
-    System.out.println(value_char);
+    int num1 = 80;
+    int num2 = Integer.parseInt("1110010",2);
+    int num3 = 111;
+    int num4 = Integer.parseInt("67",16);//67(Hex)=103
+    int num5 = Integer.parseInt("61",8);//61(Oct)=49
+
+    char a  = (char)num1;
+    char b  = (char)num2;
+    char c  = (char)num3;
+    char d  = (char)num4;
+    char e  = (char)num5;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
   }
 }
