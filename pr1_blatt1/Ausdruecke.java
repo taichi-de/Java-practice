f0a09633ec1f369ff88c0c12f35899415d4096b2
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-10-21 06:07:58 +0200

    final

--- pr1_blatt1/Ausdruecke.java
+++ pr1_blatt1/Ausdruecke.java
@@ -10,10 +10,7 @@ public class Ausdruecke {
     System.out.println(i << 1); // 22: 1011(11) -> 10110(22)
     System.out.println(i); // 11
     System.out.println(Integer.MAX_VALUE + 1 > 0 && ++i > 1); // F: F&&T=F
+    System.out.println(true && ++i > 1);
     System.out.println(i); // 11
-    // System.out.println(++i); // 12
-    // System.out.println(i); // 12
-    // System.out.println(i++); // 12
-    // System.out.println(i); // 13
   }
 }
--- pr1_blatt1/Umwandlung.java
+++ pr1_blatt1/Umwandlung.java
@@ -2,17 +2,17 @@ package pr1_blatt1;
 
 public class Umwandlung {
   public static void main(String[] args) {
-    int num1 = 80;
-    int num2 = (byte)0b1110010;
-    int num3 = 111;
-    int num4 = 0x67;
-    int num5 = 061;
+    char num1 = 80;
+    char num2 = 0b1110010;
+    char num3 = 111;
+    char num4 = 0x67;
+    char num5 = 061;
 
-    char a  = (char)num1;
-    char b  = (char)num2;
-    char c  = (char)num3;
-    char d  = (char)num4;
-    char e  = (char)num5;
+    char a  = num1;
+    char b  = num2;
+    char c  = num3;
+    char d  = num4;
+    char e  = num5;
 
     System.out.println(a);
     System.out.println(b);
