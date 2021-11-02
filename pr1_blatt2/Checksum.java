63ba392f19955316db8bbc87172a21c46614b68c
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-11-02 18:56:41 +0100

    Checksum

--- pr1_blatt2/Checksum.java
+++ pr1_blatt2/Checksum.java
@@ -1,39 +1,19 @@
 package pr1_blatt2;
 
-import java.io.* ;
-
 public class Checksum {
-  public static int quersumme ( String numberstring ) {
-    int sum = 0 ;
-    for ( int i = 0 ; i < numberstring.length( ) ; i++ )
-      sum += Integer.parseInt( numberstring.substring( i , i + 1 ) ) ;
-    return sum ;
-  }
-
-  public static void main ( String[ ] args ) {
-    System.out.print( "Bitte geben Sie eine Zahl ein:" ) ;
-    System.out.print( "Sie haben folgenden Integer eingegeben: ") ;
-    try {
-      BufferedReader in = new BufferedReader( new InputStreamReader ( System.in ) )  ;
-      String numberstring = in.readLine( ) ;
-      in.close( ) ;
-      System.out.print( "Quersumme: " ) ;
-      for ( int i = 0 ; i < numberstring.length( ) ; i++ ) {
-        System.out.print ( numberstring.substring( i , i + 1 ) ) ;
-        if ( i == numberstring.length( ) - 1 ) {
-          System.out.print( " = " ) ;
-          System.out.println( quersumme( numberstring ) ) ;
-        }
-        else System.out.print( " + " ) ;
+  public static void main(String[] args) throws Exception {
+    int input, zahl;
+    int summe = 0;
+    System.out.println("Gib ein: ");
+    do {
+      input = System.in.read();
+      if (input >=48 && input < 58) {
+        zahl = Integer.parseInt(String.valueOf(Character.toChars(input)));
+        System.out.print(zahl);
+        summe = summe + zahl;
       }
-    } catch ( IOException e ) {
-    System.err.println( e.toString( ) ) ;
     }
+    while (input >=48 && input < 58);
+    System.out.println("\nDie Quersumme ist: " + summe);
   }
 }
-
-// int i = 1;
-// do {
-//   System.out.println();
-//   i++;
-// } while (i < 10);
