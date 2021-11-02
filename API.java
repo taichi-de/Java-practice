3d61654f7c48bebae4b2d39d5144c54538263523
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-11-02 17:42:30 +0100

    Set

--- API.java
+++ API.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class API {
   public static void main(String[] args) {
+    //Array
     ArrayList<String> names = new ArrayList<String>();
     // ArrayList<String> names = new ArrayList<>(); 省略可
     names.add("Tanaka");
@@ -20,6 +21,28 @@ public class API {
     while (it.hasNext()){
       String e = it.next();
       System.out.println(e);
+    }
+
+    //Set
+    Set<String> colors = new HashSet<String>();
+    colors.add("red");
+    colors.add("yellow");
+    colors.add("blue");
+    colors.add("blue"); // colors.size() => 3
+
+    //Map
+    Map<String, Integer> prefs = new HashMap<String, Integer>();
+    prefs.put("Kyoto", 255);
+    prefs.put("Tokyo", 1250);
+    prefs.put("Kumamoto", 180);
+    int tokyo = prefs.get("Tokyo");
+    System.out.println("Tokyo's population is " + tokyo);
+    prefs.remove("Kumamoto");
+
+    for(String key: prefs.keySet()){
+      // 順序バラバラ
+      int value = prefs.get(key);
+      System.out.println(key + "'s population is " + value);
     }
   }
 }
