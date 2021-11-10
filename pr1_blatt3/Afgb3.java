9b6cd787c56bdcf17db83c0368262c36a5da082b
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-11-10 11:26:46 +0100

    pr1_blatt3 final

--- pr1_blatt3/Afgb3.java
+++ pr1_blatt3/Afgb3.java
@@ -1,12 +1,27 @@
 package pr1_blatt3;
 
 public class Afgb3 {
-  // int[] beforeArr = {10, 20, 44, 10, 1, 1, 10, 4, 11, 45, 8};
-  // int[] AfterArr = null;
+  public static void main(String[] args){
+    int[] arr = new int[]{1, 1, 3, 7, 7, 8, 9, 9, 9, 10};
+    int current = arr[0];
+    boolean found = false;
 
-  // for (int i : someArray)
-  //   sum += i;
-  // System.out.println("sum is " + sum);
+    // Original array:
+    for(int i = 0; i < arr.length; i++)
+      System.out.print(arr[i] + ", ");
+    System.out.println();
 
+    // Array without duplicates:
+    for (int i = 0; i < arr.length; i++) {
+      if (current == arr[i] && !found) {
+        found = true;
+      } else if (current != arr[i]) {
+        System.out.print(current + ", ");
+        current = arr[i];
+        found = false;
+      }
+    }
+    System.out.println(current);
+  }
 }
 
--- pr1_blatt3/Afgb4.java
+++ pr1_blatt3/Afgb4.java
@@ -1,5 +1,44 @@
 package pr1_blatt3;
 
 public class Afgb4 {
+  public static void main(String[] args) {
+    int[] arr = new int[]{0, 1, 2, 3, 3, 3, 4, 4, 5, 6, 7, 6, 5, 5, 5, 4, 3, 2, 1, 0};
+    int top = 0;
 
+    for(int i = 0; i < arr.length; i++){
+      if(top < arr[i]){
+        top = arr[i];
+      }
+    }
+
+    char mountain[][] = new char[arr.length][top+1]; // 0〜top
+
+    for(int i = 0; i < mountain.length; i++){
+      for(int j = 0; j < mountain[0].length; j++){
+        mountain[i][j]=' ';
+      }
+    }
+
+    for(int i = 0; i < arr.length; i++){
+      char symbol =  '-';
+      if(i != arr.length-1){
+        if(i != 0 && arr[i]>arr[i-1] && arr[i]>arr[i+1])
+          symbol = '^';
+        else if(arr[i]<arr[i+1])
+          symbol =  '/';
+        else if(arr[i]==arr[i+1] || arr[i]==arr[arr.length-1])
+          symbol = '-';
+        else if(arr[i]>arr[i+1])
+          symbol = '\\';
+      }
+      mountain[i][arr[i]] = symbol;
+    }
+
+    for(int j = mountain[0].length-1; j >= 0; j--){
+      for(int i = 0; i < mountain.length; i++){
+        System.out.print(mountain[i][j]);
+      }
+      System.out.println();
+    }
+  }
 }
