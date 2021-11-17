95725d17103fd4f1500859aa72f16c482f8762f0
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-11-17 11:09:24 +0100

    Afgb5-2 finito

--- pr1_blatt5/Afgb2.java
+++ pr1_blatt5/Afgb2.java
@@ -1,5 +1,24 @@
 package pr1_blatt5;
 
 public class Afgb2 {
-
+  public static int PotenzIterativ(int n) {
+    int a = 2;
+    int result = 1;
+    for(int i=1; i<=n; i++)
+      result *= a;
+    return result;
+  }
+  public static int PotenzRekursiv(int n) {
+    int a = 2;
+    int result = 1;
+    if (n > 1){
+      result = a * PotenzIterativ(n-1);
+    }
+    return result;
+  }
+  public static void main(String[] args) {
+    System.out.println("PotenzIterativ: " + PotenzIterativ(5));
+    System.out.println("PotenzRekursiv: " + PotenzRekursiv(5));
+    System.out.println("Math.pow: " + (int)Math.pow(2,5));
+  }
 }
