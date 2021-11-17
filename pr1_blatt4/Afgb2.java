fff8d455bea4771d01f365202e2f50065e8e04ac
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-11-17 09:51:52 +0100

    Afgb2 final

--- pr1_blatt4/Afgb2.java
+++ pr1_blatt4/Afgb2.java
@@ -1,44 +1,54 @@
 package pr1_blatt4;
 
 import java.util.Random;
+import java.util.Arrays;
 
 public class Afgb2 {
 	public static Random random = new Random();
 
 	public static int getSingleDiceRoll() {
-		return random.nextInt(6) + 1;
+		return random.nextInt(6)+1;
 	}
 
 	public static void printArray(int[] arr) {
 		for(int i = 0; i < arr.length; i++)
-      System.out.print(arr[i] + "-");
+      System.out.print(i == (arr.length - 1) ? arr[i] : arr[i]+"-");
     System.out.println();
 	}
 
 	public static int[] getAllDiceValues() {
     int[] diceRolls = new int[5];
-    for(int k = 1; k <= diceRolls.length; k++){
+    for(int k = 0; k < diceRolls.length; k++){
       diceRolls[k] = getSingleDiceRoll();
     }
+    Arrays.sort(diceRolls);
     return diceRolls;
   }
 
 	public static boolean isSmallStraight(int[] diceRolls) {
-    boolean isSmall = true;
-		for(int j = 0; j < diceRolls.length; j++){
-      if(diceRolls[j] == diceRolls[j + 1] && diceRolls[j+1] == diceRolls[j+2] && diceRolls[j+2] == diceRolls[j+3] && diceRolls[j+3] == diceRolls[j+4]) return true;
-      return false;
-    }
-    return isSmall;
+    if(diceRolls[0] == 1 && diceRolls[1] == 2 && diceRolls[2] == 3 && diceRolls[3] == 4)
+      return true;
+    else if(diceRolls[1] == 2 && diceRolls[2] == 3 && diceRolls[3] == 4 && diceRolls[4] == 5)
+      return true;
+    else if(diceRolls[2] == 3 && diceRolls[3] == 4 && diceRolls[4] == 5 && diceRolls[5] == 6)
+      return true;
+    return false;
   }
 
 	public static void main(String[] args) {
-    // System.out.println(isSmallStraight());
-    int runde = 0;
-    while(isSmallStraight(null)){
-      System.out.println(getAllDiceValues());
+    int[] dices;
+    int runde = 1;
+    boolean straight = false;
+    while(!straight){
+      System.out.print(String.format("Runde %s: ",runde));
+      dices = getAllDiceValues();
+      printArray(dices);
       runde++;
-    }System.out.println("Das war die " + runde + "te Runde!");
-    // System.out.println(String.format("%s was rolled %s times", i + 2, results[i]));
+
+      if(isSmallStraight(dices)){
+        System.out.println("Gewonnen!");
+        straight = true;
+      }
+    }System.out.println(String.format("Das war die %s-te Runde!",runde-1));
 	}
 }
