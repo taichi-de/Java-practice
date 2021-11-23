6cd9f07a57a111a0ac1fd3c5708fc011dc131c5b
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-11-23 16:52:26 +0100

    Afgb3 finito

--- pr1_blatt5/Afgb3.java
+++ pr1_blatt5/Afgb3.java
@@ -1,5 +1,23 @@
 package pr1_blatt5;
 
 public class Afgb3 {
+  public static int ggt(int x, int y) {
+    if(x == 0)
+      return y;
+    else if(x == y)
+      return x;
 
+    System.out.println("ggt(" + x + ", " + y + ")");
+    while (y != 0) {
+      if(x > y)
+        x = x - y;
+      else
+        y = y - x;
+      System.out.println(x + " " + y);
+    }
+    return x;
+  }
+  public static void main(String[] args) {
+    System.out.println(ggt(44,12));
+  }
 }
